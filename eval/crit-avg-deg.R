4b68963a273b34a856a250b5ea1bbd30e8de3897
Thomas Bläsius <thomas.blaesius@kit.edu>
2021-10-17 09:17:19 +0200

    tex plots + data

---
+++ eval/crit-avg-deg.R
@@ -0,0 +1,41 @@
+library(ggplot2)
+library(tikzDevice)
+
+source("general.R")
+
+tbl <- read.csv("data/crit_avg_deg.csv")
+max_iteration <- max(tbl$iteration)
+tbl <- tbl[tbl$iteration == max_iteration, ]
+tbl$degen <- tbl$minority_count == 0
+tbl <- aggregate(. ~ model + n + deg_avg_exp, data = tbl, mean)
+
+tbl$sqrtn <- sqrt(tbl$n)
+tbl$factor <- tbl$deg_avg_exp / tbl$sqrtn
+
+tbl_lines <- tbl[tbl$factor <= 3.2 & tbl$factor >= 1.4, ]
+tbl_labels <- tbl_lines[tbl_lines$n == 10000, ]
+tbl_lines <- tbl[tbl$factor <= 2.6 & tbl$factor >= 1.4, ]
+
+p <- ggplot(tbl,
+            aes(x = deg_avg_exp,
+                y = degen,
+                color = factor(n))) +
+    geom_line(data = tbl_lines, aes(group = factor), color = "gray") +
+    geom_line() + geom_point() +
+    geom_segment(data = tbl_labels, aes(xend = deg_avg_exp, yend = -Inf),
+                 linetype = "dashed") +
+    geom_label(data = tbl_labels,
+               aes(label = paste0("$", factor, "\\sqrt{n}$"),
+                   ## y = 0.5 * degen - 0.01,
+                   y = (factor - 1.4) / 0.3 / 15),
+               show.legend = FALSE, size = 3.1) +
+    scale_color_manual(values = colors) +
+    xlab("expected average degree") +
+    ylab(paste("probability to become monocolored\n(after",
+               max_iteration, "iterations)")) +
+    scale_color_manual(name = "n", values = rev(colors))
+ggsave("pdf/crit-avg-deg.pdf", plot = p, width = 6, height = 4)
+
+tikz(file = "tex/crit-avg-deg.tex", width = 5.5, height = 2.8)
+print(p)
+dev.off()
--- eval/extended.R
+++ eval/extended.R
@@ -11,18 +11,28 @@ tbl <- tbl[tbl$iteration <= 25, ]
 p1 <- main_plot(tbl,
                 aes(x = iteration,
                     y = monochrome_fraction,
-                    color = factor(deg_avg_exp)))
+                    color = factor(deg_avg_exp))) +
+    ylab("fraction of monochrome edges") +
+    scale_color_manual(name = "average\ndegree", values = colors)
 
 p2 <- main_plot(tbl,
                 aes(x = iteration,
                     y = minority_count_fraction,
-                    color = factor(deg_avg_exp)))
+                    color = factor(deg_avg_exp))) +
+    ylab("fraction of vertices\nbelonging to the minority") +
+    scale_color_manual(name = "average\ndegree", values = colors)
 
 p3 <- main_plot(tbl[tbl$iteration > 0, ],
                 aes(x = iteration,
                     y = color_changes_fraction,
-                    color = factor(deg_avg_exp)))
+                    color = factor(deg_avg_exp))) +
+    ylab("fraction of vertices changing color") +
+    scale_color_manual(name = "average\ndegree", values = colors)
 
 p <- ggarrange(p1, p2, p3, nrow = 3, ncol = 1)
 
 ggsave("pdf/extended-plot.pdf", plot = p, width = 9, height = 9)
+
+tikz(file = "tex/extended-plot.tex", width = 5.5, height = 6.5)
+print(p)
+dev.off()
